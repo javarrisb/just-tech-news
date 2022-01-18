bac7eb71ed46db10bea5ba1d74f4c086bf58ee1d
Javarris Barnett <javarrisb@gmail.con>
2022-01-18 16:33:42 +0100

    Update

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -2,22 +2,21 @@ async function commentFormHandler(event) {
     event.preventDefault();
 
     const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
-
-    const post_id = window.location.toString().split('/')[window.location.toString().split('/').length - 1
+    const post_id = window.location.toString().split('/')[
+        window.location.toString().split('/').length - 1
     ];
 
     if (comment_text) {
-        const response = await fetch('/api/comments',
-            {
-                method: 'POST',
-                body: JSON.stringify({
-                    post_id,
-                    comment_text
-                }),
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            });
+        const response = await fetch('/api/comments', {
+            method: 'POST',
+            body: JSON.stringify({
+                post_id,
+                comment_text
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
 
         if (response.ok) {
             document.location.reload();
